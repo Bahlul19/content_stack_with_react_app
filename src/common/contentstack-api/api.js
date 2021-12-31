536b371cdb732398f8a17768d3c39b196f90353b
Bahlul19 <bahlul.tausif@gmail.com>
2021-12-31 18:22:20 +0600

    having issue with front-end value fecth from the code

---
+++ src/common/contentstack-api/api.js
@@ -0,0 +1,4 @@
+import Contentstack from 'contentstack';
+
+export const Stack = Contentstack.Stack('blt1ffa4652a21e7445', 'csa7a3e42e1808107a1098465e', 'development')
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,52 @@
+
+import React, {Component} from 'react'
+import '../styles/styles.css'
+import { Link } from 'react-router-dom'
+import { Stack } from '../common/contentstack-api/api'
+
+export default class Home extends Component{
+    constructor() {
+      super()
+      this.state = { loading : true, result: null}
+    }
+    componentDidMount () {
+      var  Query = Stack.ContentType("categories").Query()
+      .toJSON()
+      .find()
+      .then((result) => {
+        this.setState({loading : false, result:result})
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }
+  
+    renderList (post) {
+      return (
+        <main>
+        <div className="media-body ">
+          {
+            post.map((value, index) => {
+              return (
+              <article className="content-section">
+                <div className="article-metadata">
+                  <Link to={`/detail/${value.uid}`}>
+                     <h3 className="article-title">{ value.title }</h3>
+                  </Link>
+                  <div dangerouslySetInnerHTML={{ __html: value.body} } />
+                </div>
+              </article>
+                 )
+            })
+          }
+        </div>
+        </main>
+      )
+    }
+
+  
+    render () {
+      const {loading, result} = this.state
+      return (this.state.loading) ? <h1>loading...</h1> : this.renderList(result[0])
+    }
+  }
